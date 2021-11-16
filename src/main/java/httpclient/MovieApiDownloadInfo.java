0f031cef71488040603cd766c0c2bff9255fe519
Nadya <batiskaf2017@gmail.com>
2021-11-16 22:17:03 +0200

    get all films

--- src/main/java/httpclient/MovieApiDownloadInfo.java
+++ src/main/java/httpclient/MovieApiDownloadInfo.java
@@ -1,5 +1,6 @@
 package httpclient;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import httpclient.model.Movie;
@@ -8,21 +9,59 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class MovieApiDownloadInfo {
+
     public static void main(String[] args) throws IOException {
         OkHttpClient client = new OkHttpClient();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String root = "https://api.themoviedb.org/3/discover/movie";
+        Map<String, String> queryParameters = new HashMap<>();
+        queryParameters.put("api_key", "50e28c2946dd6d45673965da6dadcf42");
 
-        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://api.themoviedb.org/3/discover/movie").newBuilder();
-        urlBuilder.addQueryParameter("api_key", "50e28c2946dd6d45673965da6dadcf42");
-        String url = urlBuilder.build().toString();
+        JsonNode parent = objectMapper.readTree(info(client, root, queryParameters).body().string());
+        int pagesCount = parent.path("total_pages").asInt();
 
+        for (int i = 1; i <= pagesCount; i++) {
+            queryParameters.put("page", String.valueOf(i));
+            parent = objectMapper.readTree(info(client, root, queryParameters).body().string());
+            String content = parent.path("results").toString();
+            Movie[] movieP = objectMapper.readValue(content, Movie[].class);
+            for (Movie movie : movieP) {
+                System.out.println(movie);
+            }
+        }
+    }
+
+    public static Response info(OkHttpClient client, String urlApi, Map<String, String> queryParameters) throws IOException {
+        HttpUrl.Builder url = HttpUrl.parse(urlApi).newBuilder();
+        Iterator<Map.Entry<String, String>> parameters = queryParameters.entrySet().iterator();
+        while (parameters.hasNext()) {
+            Map.Entry<String, String> param = parameters.next();
+            url.addQueryParameter(param.getKey(), param.getValue());
+        }
+        String urlFull = url.build().toString();
         Request request = new Request.Builder()
-                .url(url)
+                .url(urlFull)
                 .build();
-        Response response =null;
-        for (int i=0; i<10;i++) {
+        Response response = response(client, request);
+        if (response == null)
+            throw new IllegalArgumentException("No response after 10 times!");
+
+        if (response.body() == null)
+            throw new IllegalArgumentException("body of response didn't have info");
+        return response;
+    }
+
+    public static Response response(OkHttpClient client, Request request) {
+        Response response = null;
+        for (int i = 0; i < 10; i++) {
             try {
                 response = client.newCall(request).execute();
                 break;
@@ -30,16 +69,12 @@ public class MovieApiDownloadInfo {
                 e.printStackTrace();
             }
         }
-        if (response==null)
-            throw new IllegalArgumentException("No response after 10 times!");
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode parent= objectMapper.readTree(response.body().string());
-        String content = parent.path("results").toString();
-        Movie[] movie = objectMapper.readValue(content, Movie[].class);
-        System.out.println(movie.length);
-        for (Movie movie1 : movie) {
-            System.out.println(movie1);
-        }
+        return response;
     }
 }
+/*
+to write info to comp
+    BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\nadya\\Desktop\\oldComp\\task4.txt", true));
+            writer.append(' ');
+                    writer.append(content);
+                    writer.close();*/
