1655f8256fd448444573bd70665fdfc73d841d50
Nadya <batiskaf2017@gmail.com>
2021-11-18 15:06:00 +0200

    dowload all images from Api

--- src/main/java/httpclient/MovieApiDownloadInfo.java
+++ src/main/java/httpclient/MovieApiDownloadInfo.java
@@ -1,6 +1,5 @@
 package httpclient;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import httpclient.model.Movie;
@@ -9,37 +8,86 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
 public class MovieApiDownloadInfo {
 
+    static String baseUrl;
+    static String posterSize;
+    static String root = "https://api.themoviedb.org/3";
+    static String apiKey = "50e28c2946dd6d45673965da6dadcf42";
+    static ObjectMapper objectMapper = new ObjectMapper();
+
     public static void main(String[] args) throws IOException {
         OkHttpClient client = new OkHttpClient();
-        ObjectMapper objectMapper = new ObjectMapper();
-        String root = "https://api.themoviedb.org/3/discover/movie";
         Map<String, String> queryParameters = new HashMap<>();
-        queryParameters.put("api_key", "50e28c2946dd6d45673965da6dadcf42");
-
-        JsonNode parent = objectMapper.readTree(info(client, root, queryParameters).body().string());
+        queryParameters.put("api_key", apiKey);
+        JsonNode parent = objectMapper.readTree(getHttpBody(client, root + "/discover/movie", queryParameters));
         int pagesCount = parent.path("total_pages").asInt();
+        queryParameters.remove("page");
+        parent = objectMapper.readTree(getHttpBody(client, root + "/configuration", queryParameters));
+        baseUrl = parent.path("images").path("base_url").asText();
+        Iterator<JsonNode> elements = parent.path("images").withArray("poster_sizes").elements();
 
+        while (elements.hasNext()) {
+            String tmpSize = elements.next().asText();
+            if (Integer.valueOf(tmpSize.substring(1)) > 300) {
+                posterSize = tmpSize;
+                break;
+            }
+        }
+        Movie[] movieP = null;
         for (int i = 1; i <= pagesCount; i++) {
-            queryParameters.put("page", String.valueOf(i));
-            parent = objectMapper.readTree(info(client, root, queryParameters).body().string());
-            String content = parent.path("results").toString();
-            Movie[] movieP = objectMapper.readValue(content, Movie[].class);
+            movieP = getArrayOfFilmsFromPages(client, root, queryParameters, i);
             for (Movie movie : movieP) {
-                System.out.println(movie);
+                File tempFile = new File("C:\\Users\\nadya\\Desktop\\imges\\" + movie.getPosterPath());
+                if (tempFile.exists()) {
+                    continue;
+                }
+                OutputStream outputStream = new FileOutputStream("C:\\Users\\nadya\\Desktop\\imges\\" + movie.getPosterPath());
+                InputStream inputStream = getHttpBodyInputStream(client, baseUrl + posterSize + "/" + movie.getPosterPath(), queryParameters);
+                copy(inputStream, outputStream);
+                outputStream.close();
             }
         }
     }
 
-    public static Response info(OkHttpClient client, String urlApi, Map<String, String> queryParameters) throws IOException {
+    static Movie[] getArrayOfFilmsFromPages(OkHttpClient client, String urlApi, Map<String, String> queryParameters, int numberOfPage) throws IOException {
+        queryParameters.put("page", String.valueOf(numberOfPage));
+        JsonNode parent = objectMapper.readTree(getHttpBody(client, urlApi + "/discover/movie", queryParameters));
+        String content = parent.path("results").toString();
+        Movie[] movieArray = objectMapper.readValue(content, Movie[].class);
+        return movieArray;
+    }
+
+    static void copy(InputStream source, OutputStream target) throws IOException {
+        byte[] buf = new byte[8192];
+        int length;
+        while ((length = source.read(buf)) > 0) {
+            target.write(buf, 0, length);
+        }
+    }
+
+    static String getHttpBody(OkHttpClient client, String urlApi, Map<String, String> queryParameters) throws IOException {
+        Response response = makeGetRequest(client, urlApi, queryParameters);
+        if (response.body() == null)
+            throw new IllegalArgumentException("body of response didn't have info");
+
+        return response.body().string();
+    }
+
+    static InputStream getHttpBodyInputStream(OkHttpClient client, String urlApi, Map<String, String> queryParameters) throws IOException {
+        Response response = makeGetRequest(client, urlApi, queryParameters);
+        if (response.body() == null)
+            throw new IllegalArgumentException("body of response didn't have info");
+
+        return response.body().byteStream();
+    }
+
+    static Response makeGetRequest(OkHttpClient client, String urlApi, Map<String, String> queryParameters) throws IOException {
         HttpUrl.Builder url = HttpUrl.parse(urlApi).newBuilder();
         Iterator<Map.Entry<String, String>> parameters = queryParameters.entrySet().iterator();
         while (parameters.hasNext()) {
@@ -50,16 +98,13 @@ public class MovieApiDownloadInfo {
         Request request = new Request.Builder()
                 .url(urlFull)
                 .build();
-        Response response = response(client, request);
+        Response response = getResponse(client, request);
         if (response == null)
             throw new IllegalArgumentException("No response after 10 times!");
-
-        if (response.body() == null)
-            throw new IllegalArgumentException("body of response didn't have info");
         return response;
     }
 
-    public static Response response(OkHttpClient client, Request request) {
+    static Response getResponse(OkHttpClient client, Request request) {
         Response response = null;
         for (int i = 0; i < 10; i++) {
             try {
@@ -72,9 +117,4 @@ public class MovieApiDownloadInfo {
         return response;
     }
 }
-/*
-to write info to comp
-    BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\nadya\\Desktop\\oldComp\\task4.txt", true));
-            writer.append(' ');
-                    writer.append(content);
-                    writer.close();*/
+
